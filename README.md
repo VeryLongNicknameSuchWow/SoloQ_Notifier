6109f12101511fddacca1c84e2c4009033029f04
VeryLongNicknameSuchWow <d33p4ck@gmail.com>
2023-06-26 14:00:05 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,65 @@
+# Solo Queue Notifier
+
+This script is a simple tool to track League of Legends games of a specific player and send notifications
+about their games to a Discord channel. It utilizes Riot Games API to fetch game data and uses webhooks to send messages
+to Discord.
+
+### Features
+- Sends a Discord message whenever the player starts a new game
+- Sends a Discord message whenever the player finishes a game
+
+### Dependencies
+
+This script was tested on Python 3.10 and requires the `requests` library. You can install it with pip:
+
+```
+pip3 install requests
+```
+
+### Configuration
+
+Before running the script, a `config.ini` file needs to be created in the same directory as the script. The file should
+contain a section `SOLOQ` with the following keys:
+
+- `RIOT_API_KEY`: Your Riot Games API Key.
+- `WEBHOOK_URL`: The Discord webhook URL you want to use to send notifications.
+- `USERNAME`: The username of the player you want to track.
+- `USER_REGION`: The specific region the player plays in, e.g., `na1` for North America.
+- `WIDE_REGION`: The broad region the player is in, e.g., `americas` for North America.
+- `DATA_FILE`: The name of the data file to store game and error information.
+
+You can get your Riot Games API key [here](https://developer.riotgames.com/). User and wide regions should
+match [Riot Games API routing values](https://developer.riotgames.com/docs/lol#routing-values). Information on how to
+generate a Discord channel webhook is provided
+in [this article](https://support.discord.com/hc/en-us/articles/228383668-Intro-to-Webhooks).
+
+### Running the script
+
+This script can be run with the following command:
+
+```
+python3 soloq.py
+```
+
+You can setup a [cron](https://en.wikipedia.org/wiki/Cron) job to run the script automatically every few minutes. To do that run `crontab -e` to open the cron
+table editor and add a new line at the bottom:
+
+```
+*/1 * * * * cd /path-to-your-script && python3 soloq.py
+```
+
+This will run the script once every minute.
+
+### Note
+
+Riot Games API has strict rate limits. If your application exceeds those limits, your IP may be temporarily blacklisted.
+Always ensure you are within the API usage limits to prevent this from happening.
+
+### Disclaimer
+
+This script is not endorsed by Riot Games and does not reflect the views or opinions of Riot Games or anyone officially
+involved in producing or managing Riot Games properties. Riot Games, and all associated properties, are trademarks or
+registered trademarks of Riot Games, Inc.
+
+This script uses the Discord API but is not endorsed or certified by Discord. Discord, the Discord logo, and any
+associated marks and logos are trademarks or registered trademarks of Discord, Inc., in the U.S. and other countries.
---
+++ soloq.py
@@ -0,0 +1,120 @@
+import configparser
+import requests
+import shelve
+
+config = configparser.ConfigParser()
+if not config.read('config.ini'):
+    print("Could not load config.ini file")
+    exit(1)
+
+try:
+    config_section = config['SOLOQ']
+except KeyError as e:
+    print("Config file must contain a [SOLOQ] section")
+    exit(1)
+
+try:
+    RIOT_API_KEY = config_section['RIOT_API_KEY']
+    WEBHOOK_URL = config_section['WEBHOOK_URL']
+    USERNAME = config_section['USERNAME']
+    USER_REGION = config_section['USER_REGION']
+    WIDE_REGION = config_section['WIDE_REGION']
+    DATA_FILE = config_section['DATA_FILE']
+except KeyError as e:
+    print(e, "must be specified in config [SOLOQ] section")
+    exit(1)
+
+
+def get_summoner_dto():
+    summoner_by_name_url = f"https://{USER_REGION}.api.riotgames.com/lol/summoner/v4/summoners/by-name/{USERNAME}"
+    response = requests.get(summoner_by_name_url, params={'api_key': RIOT_API_KEY})
+    summoner_dto = response.json()
+    if not response.ok:
+        raise Exception("Could not get summoner", summoner_dto)
+    return summoner_dto
+
+
+def notify_game_result(summoner_dto, data):
+    puuid = summoner_dto['puuid']
+    last_match = data['last_match']
+
+    matches_by_puuid_url = f"https://{WIDE_REGION}.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids"
+    response = requests.get(matches_by_puuid_url, params={'api_key': RIOT_API_KEY})
+    matches_dto = response.json()
+    if not response.ok:
+        raise Exception("Could not get match history", matches_dto)
+    if not matches_dto:
+        print("Match history is empty")
+        return
+
+    for match in matches_dto:
+        if match == last_match:
+            break
+
+        match_by_id_url = f"https://{WIDE_REGION}.api.riotgames.com/lol/match/v5/matches/{match}"
+        response = requests.get(match_by_id_url, params={'api_key': RIOT_API_KEY})
+        match_dto = response.json()
+        if not response.ok:
+            raise Exception("Could not get match details", match_dto)
+
+        participants = match_dto['info']['participants']
+
+        for p in participants:
+            if p['puuid'] == puuid:
+                result = "won" if p['win'] else "lost"
+                message = f"{USERNAME} {result} a game (ID: {match})"
+                print(message)
+                response = requests.post(WEBHOOK_URL, json={'content': message})
+                if not response.ok:
+                    raise Exception("Could not post to Discord")
+                break
+
+    data['last_match'] = str(matches_dto[0])
+
+
+def notify_in_game(summoner_dto, data):
+    summoner_eid = summoner_dto['id']
+
+    active_games_url = f"https://{USER_REGION}.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/{summoner_eid}"
+    response = requests.get(active_games_url, params={'api_key': RIOT_API_KEY})
+    current_game_info = response.json()
+    if not response.status_code == 404 and not response.ok:
+        raise Exception("Could not get active game", current_game_info)
+
+    if 'status' in current_game_info:
+        print("Currently not in game")
+        return
+
+    current_game = str(current_game_info['gameId'])
+    previous_game = data['in_game']
+
+    if previous_game == current_game:
+        print(f"In the same game (ID: {current_game})")
+        return
+
+    message = f"{USERNAME} started a new game (ID: {current_game})"
+    print(message)
+    response = requests.post(WEBHOOK_URL, json={'content': message})
+    if not response.ok:
+        raise Exception("Could not post to discord")
+
+    data['in_game'] = str(current_game)
+
+
+if __name__ == '__main__':
+    with shelve.open(DATA_FILE) as data:
+        for key in ['in_game', 'last_match', 'error']:
+            if key not in data:
+                data[key] = ''
+
+        try:
+            summoner = get_summoner_dto()
+            notify_game_result(summoner, data)
+            notify_in_game(summoner, data)
+            data['error'] = False
+            print("Ran successfully!")
+        except Exception as e:
+            if not data['error']:
+                requests.post(WEBHOOK_URL, json={'content': str(e)})
+            data['error'] = True
+            raise e
