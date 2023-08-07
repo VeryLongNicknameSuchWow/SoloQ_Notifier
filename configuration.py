02fc210453d32f6d301a5d8f140c06529e555e37
VeryLongNicknameSuchWow <d33p4ck@gmail.com>
2023-08-07 19:05:34 +0200

    Type assertions, redis configuration

--- configuration.py
+++ configuration.py
@@ -5,19 +5,17 @@ logger = logging.getLogger(__name__)
 
 config = configparser.ConfigParser()
 config_structure = {
-    'DISCORD': [
-        'BOT_TOKEN',
-    ],
-    'RIOT': [
-        'API_KEY',
-    ],
-    'MONGO': [
-        'DB_USER',
-        'DB_PASSWORD',
-        'DB_HOST',
-        'DB_PORT',
-        'DATABASE',
-    ],
+    'DISCORD': {
+        'BOT_TOKEN': str,
+    },
+    'RIOT': {
+        'API_KEY': str
+    },
+    'REDIS': {
+        'HOST': str,
+        'PORT': int,
+        'DB': int,
+    },
 }
 
 
@@ -27,18 +25,28 @@ def read_config(config_path):
         exit(1)
 
     success = True
+    config_data = {}
     for section in config_structure:
         if section not in config:
             success = False
             logger.error(f"Section '{section}' not found in '{config_path}' file")
 
         config_section = config[section]
-        for key in config_structure[section]:
+        section_data = {}
+        for key, data_type in config_structure[section].items():
             if key not in config_section:
                 success = False
                 logger.error(f"Field '{key}' not found in '{section}' section of '{config_path}' file")
+                continue
 
+            try:
+                section_data[key] = data_type(config_section[key])
+            except ValueError:
+                success = False
+                logger.error(f"Field '{key}' in '{section}' section could not be read as '{data_type.__name__}'")
+
+        config_data[section] = section_data
     if not success:
-        raise Exception("Configuration is incomplete")
+        raise Exception("Configuration is incomplete or incorrect")
 
-    return config
+    return config_data
